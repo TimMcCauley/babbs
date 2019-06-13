b84682bd5bb36d527248cb2ae1be7f898e2688bb
Dylan Babbs <dylan.babbs@here.com>
2019-06-13 10:21:18 +0800

    add isoline routing information

--- src/Containers/Post/SeattleJSIsolineRouting/SeattleJSIsolineRouting.js
+++ src/Containers/Post/SeattleJSIsolineRouting/SeattleJSIsolineRouting.js
@@ -7,6 +7,15 @@ export const SeattleJSIsolineRouting = () => {
       <p>
       A few weeks ago I spoke at the Seattle JavaScript meetup about isoline routing.
       </p>
+      <p>
+         <a href="https://dbabbs.github.io/isolines/">View the live demo</a>
+      </p>
+      <p>
+         <a href="https://github.com/dbabbs/isolines/blob/master/isolines.pdf">View the deck</a>
+      </p>
+      <p>
+         <a href="https://github.com/dbabbs/isolines">View the source code for the demo</a>
+      </p>
       <iframe 
          width="100%" 
          height="600" 
